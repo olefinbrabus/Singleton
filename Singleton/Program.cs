a44f49a7a6a87bd982bef74296418280b6a700e4
Oleg Filipenkov <olefinibrabus@gmail.com>
2023-12-05 14:51:55 +0200

    first commit

---
+++ Singleton/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public sealed class ApiKeySingleton
+{
+    private static ApiKeySingleton instance;
+    private string apiKey;
+
+    private ApiKeySingleton()
+    {
+        apiKey = "your_api_key_here";
+    }
+
+    public static ApiKeySingleton Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new ApiKeySingleton();
+            }
+            return instance;
+        }
+    }
+
+    public string ApiKey
+    {
+        get { return apiKey; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ApiKeySingleton apiKeySingleton = ApiKeySingleton.Instance;
+
+        string apiKey = apiKeySingleton.ApiKey;
+        
+        Console.WriteLine("API Key: " + apiKey);
+    }
+}
